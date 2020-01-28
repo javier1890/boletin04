032e81f410f108a8e4c8841ff403520b698e30d2
javier1890 <60121488+javier1890@users.noreply.github.com>
2020-01-28 00:32:42 -0300

    subiendo ejercicio medio

--- Ejercicio09.java
+++ Ejercicio09.java
@@ -1,11 +1,53 @@
 package boletin04;
 
+import java.util.Scanner;
+
 public class Ejercicio09 {
 
 	public static void main(String[] args) {
-		//crear un programa que lea por teclado una tabla de 10 nyumeros y la desplace una posicion hacia abajo 
-		//(el ultimo pasa a ser el primero)
 
+		// crear un programa que lea por teclado una tabla de 10 nyumeros y la desplace
+		// una posicion hacia abajo
+		// (el ultimo pasa a ser el primero)
+		Scanner leer = new Scanner(System.in);
+        int[] tabla=new int[10];
+        
+		System.out.println("Tabla A:");
+		for (int i = 0; i <= 9; i++) {
+			System.out.println((i + 1) + ")ingresar el valor:");
+			tabla[i] = leer.nextInt();
+	
+		}
+		
+		System.out.println("Mostrando Tabla A:");
+		for (int i = 0; i <= 9; i++) {
+			System.out.println(tabla[i]);
+		}
+		
+		
+		int aux,aux2;
+		
+		System.out.println("Mostrando Tabla A:");
+		for (int i = 0; i <= 9; i++) {
+			
+			if ((tabla.length-1)==9) {
+				aux=tabla[tabla.length-1];
+				aux2=tabla[0];
+				
+				
+				tabla[0]=aux;
+				tabla[1]=aux2;
+			}else {
+				aux=tabla[i];
+		    	tabla[i+1]=aux;
+				
+//				aux = tabla[i + 1];
+//                tabla[i + 1] = tabla[i];
+//                tabla[i] = aux;
+			}
+			System.out.println(tabla[i]);
+		}
+		
 	}
 
 }
